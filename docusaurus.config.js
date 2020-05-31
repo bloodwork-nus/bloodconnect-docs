fd5d6cbe0049d13e1c9660c6e0f7dc6279326362
Phillmont Muktar <51525686+purfectliterature@users.noreply.github.com>
2020-06-01 03:28:14 +0800

    Fixed Edit this page URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,13 +93,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/bloodwork-nus/bloodconnect-docs/edit/master/website/',
+            'https://github.com/bloodwork-nus/bloodconnect-docs/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/bloodwork-nus/bloodconnect-docs/edit/master/website/blog/',
+            'https://github.com/bloodwork-nus/bloodconnect-docs/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
